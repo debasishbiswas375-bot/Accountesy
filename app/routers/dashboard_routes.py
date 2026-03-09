0c87f8d2a10bb2096570b6c29edaad867e987f81
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-09 14:23:27 +0530

    Update dashboard_routes.py

--- app/routers/dashboard_routes.py
+++ app/routers/dashboard_routes.py
@@ -2,20 +2,20 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+
 templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/dashboard")
 def dashboard(request: Request):
 
-    user = {
-        "name":"Demo User",
-        "credits":25.5,
-        "vouchers":255,
-        "plan":"Starter",
-        "expiry":"2026-03-30"
+    data = {
+        "credits": 100,
+        "plan": "Starter",
+        "expiry": "2026-04-01",
+        "conversions": 34
     }
 
     return templates.TemplateResponse(
         "dashboard.html",
-        {"request": request,"user":user}
+        {"request": request, "data": data}
     )
