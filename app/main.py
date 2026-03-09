12fdf6abe7a87f69bbb14b4f9a49241b09996312
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-09 11:59:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from app.routers import auth_routes, workspace_routes, history_routes, admin_routes
+
+from app.routers import landing_routes
+from app.routers import auth_routes
+from app.routers import dashboard_routes
+from app.routers import workspace_routes
+from app.routers import history_routes
+from app.routers import admin_routes
+from app.routers import plans_routes
 
 app = FastAPI(title="Accountesy – Accounting Expert")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-templates = Jinja2Templates(directory="app/templates")
-
+app.include_router(landing_routes.router)
 app.include_router(auth_routes.router)
+app.include_router(dashboard_routes.router)
 app.include_router(workspace_routes.router)
 app.include_router(history_routes.router)
 app.include_router(admin_routes.router)
-
-@app.get("/")
-def landing(request: Request):
-    return templates.TemplateResponse("landing.html", {"request": request})
+app.include_router(plans_routes.router)
