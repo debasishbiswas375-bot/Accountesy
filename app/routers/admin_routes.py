3f730d63a8490ccdbce53506f0cceef15572f518
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-09 14:24:58 +0530

    Update admin_routes.py

--- app/routers/admin_routes.py
+++ app/routers/admin_routes.py
@@ -1,6 +1,21 @@
-from fastapi import APIRouter
-router=APIRouter()
+from fastapi import APIRouter, Form
 
-@router.get("/admin")
-def admin():
-    return {"admin":"panel"}
+router = APIRouter(prefix="/admin")
+
+@router.get("/")
+def admin_dashboard():
+
+    return {
+        "users":120,
+        "conversions":560
+    }
+
+@router.post("/user/update")
+
+def update_user(
+    user_id:int = Form(...),
+    email:str = Form(...),
+    credits:int = Form(...)
+):
+
+    return {"status":"user updated"}
