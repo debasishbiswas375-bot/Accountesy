191a9f1b49ef6aa1a29f8dfde31cc171730c8b3f
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-12 19:19:36 +0530

    Update dashboard.py

--- Accountesy/app/routers/dashboard.py
+++ Accountesy/app/routers/dashboard.py
@@ -1,11 +1,31 @@
-﻿from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+
 @router.get("/dashboard", response_class=HTMLResponse)
 def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
 
+
+@router.get("/workspace", response_class=HTMLResponse)
+def workspace(request: Request):
+    return templates.TemplateResponse("workspace.html", {"request": request})
+
+
+@router.get("/history", response_class=HTMLResponse)
+def history(request: Request):
+    return templates.TemplateResponse("history.html", {"request": request})
+
+
+@router.get("/pricing", response_class=HTMLResponse)
+def pricing(request: Request):
+    return templates.TemplateResponse("pricing.html", {"request": request})
+
+
+@router.get("/account", response_class=HTMLResponse)
+def account(request: Request):
+    return templates.TemplateResponse("account.html", {"request": request})
