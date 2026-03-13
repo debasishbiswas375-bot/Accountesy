cd79e1122367e72303fa1f693df3dd420f6fd1ca
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-13 12:59:30 +0530

    Update auth.py

--- Accountesy/app/routers/auth.py
+++ Accountesy/app/routers/auth.py
@@ -1,21 +1,21 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from app.database import supabase
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/")
 def landing(request: Request):
     return templates.TemplateResponse("landing.html", {"request": request})
 
 
-@router.get("/login", response_class=HTMLResponse)
+@router.get("/login")
 def login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
-@router.get("/register", response_class=HTMLResponse)
+@router.get("/register")
 def register(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
