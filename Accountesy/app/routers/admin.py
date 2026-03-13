8e631c441c522c97152014021eed166f24d1da59
Debasish Biswas <debasish.biswas375@gmail.com>
2026-03-13 13:01:00 +0530

    Update admin.py

--- Accountesy/app/routers/admin.py
+++ Accountesy/app/routers/admin.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from app.database import supabase
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
-@router.get("/admin", response_class=HTMLResponse)
-def admin(request: Request):
-    return templates.TemplateResponse("admin.html", {"request": request})
+@router.get("/admin")
+def admin_panel(request: Request):
+
+    users = supabase.table("users").select("*").execute()
+
+    return templates.TemplateResponse(
+        "admin.html",
+        {
+            "request": request,
+            "users": users.data
+        }
+    )
